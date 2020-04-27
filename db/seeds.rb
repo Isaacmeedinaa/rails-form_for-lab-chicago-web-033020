3c57e1f06d77fc7dc912633a26a6275e0a6e239d
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-04-26 22:38:41 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Isaac", last_name: "Medina")
+Student.create(first_name: "Alex", last_name: "Pillado")
+Student.create(first_name: "Bean", last_name: "Mendoza")
+
+SchoolClass.create(title: "Software Engineering", room_number: 1)
+SchoolClass.create(title: "Cyber Security", room_number: 2)
+SchoolClass.create(title: "UI/UX Design", room_number: 3)
+SchoolClass.create(title: "Data Science", room_number: 4)
